4fe0039fb91ced6f7e9cc87891921c5453352e8f
Alejandro Marino <aldamajo1@gmail.com>
2024-12-12 20:11:05 +0100

    testing workflow

--- src/main/java/org/marino/server/domain/services/ServicesEmail.java
+++ src/main/java/org/marino/server/domain/services/ServicesEmail.java
@@ -35,8 +35,6 @@ public class ServicesEmail {
                 "</body>" +
                 "</html>";
 
-        //test
-
         helper.setText(emailBody, true);
 
         mailSender.send(message);
